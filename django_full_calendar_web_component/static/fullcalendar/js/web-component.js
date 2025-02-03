f1979f242314959bda4f11daba29cc0afc9f81fd
Pablo Androetto <androettop@gmail.com>
2025-02-03 15:29:07 +0100

    Prepare for abstraction

--- django_full_calendar_web_component/static/fullcalendar/js/web-component.js
+++ django_full_calendar_web_component/static/fullcalendar/js/web-component.js
@@ -1,3 +1,22 @@
-class CalendarViewElement extends FullCalendarElement {}
+class CalendarElement extends FullCalendarElement {
+  constructor() {
+    super(...arguments);
+  }
 
-customElements.define('calendar-view', CalendarViewElement);
+  connectedCallback() {
+    this._handleOptionsStr(this.getAttribute("options"));
+  }
+
+  _optionsToFullCalendar(options) {
+    // TODO: do the real conversion
+    console.log("options", options);
+    return options;
+  }
+
+  _handleOptions(options) {
+    const fullCalendarOptions = this._optionsToFullCalendar(options);
+    return super._handleOptions(fullCalendarOptions);
+  }
+}
+
+customElements.define("calendar-element", CalendarElement);
