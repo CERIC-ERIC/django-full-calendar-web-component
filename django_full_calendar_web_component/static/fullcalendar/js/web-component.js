d2f9eb890efb65ccf78f04632a597e6fddca1a69
Pablo Androetto <androettop@gmail.com>
2025-02-05 11:31:29 +0100

    initial events rendering

--- django_full_calendar_web_component/static/fullcalendar/js/web-component.js
+++ django_full_calendar_web_component/static/fullcalendar/js/web-component.js
@@ -3,17 +3,119 @@ class CalendarElement extends FullCalendarElement {
     super(...arguments);
   }
 
+  static EVENT_COLORS = [
+    "#2196f3",
+    "#009688",
+    "#ff9800",
+    "#9c27b0",
+    "#f44336",
+    "#673ab7",
+    "#e91e63",
+    "#03a9f4",
+    "#3f51b5",
+    "#4caf50",
+    "#ffc107",
+    "#8bc34a",
+    "#795548",
+    "#607d8b",
+    "#ff5722",
+    "#cddc39",
+  ];
+
+  static RESERVED_COLOR = "#9e9e9e";
+
+  static _getColor(index) {
+    return CalendarElement.EVENT_COLORS[
+      index % CalendarElement.EVENT_COLORS.length
+    ];
+  }
+
   connectedCallback() {
     this._handleOptionsStr(this.getAttribute("options"));
   }
 
+  _handleOptionsStr(optionsStr) {
+    if (optionsStr.startsWith("global:")) {
+      this._handleOptions(window[optionsStr.split(":")[1]]);
+    } else {
+      this._handleOptions(optionsStr ? JSON.parse(optionsStr) : null);
+    }
+  }
+
   _optionsToFullCalendar(options) {
-    // TODO: do the real conversion
-    console.log("options", options);
+    const newOptions = {
+      initialView: options.initialView,
+      views: options.views,
+      headerToolbar: options.headerToolbar,
+      nowIndicator: true,
+      slotDuration: options.slotDuration,
+      themeSystem: "bootstrap5",
+    };
 
     // Add the Non-Commercial license key
-    options.schedulerLicenseKey = "CC-Attribution-NonCommercial-NoDerivatives";
-    return options;
+    newOptions.schedulerLicenseKey =
+      "CC-Attribution-NonCommercial-NoDerivatives";
+
+    // create resources and events
+    const proposals = [];
+    const resources = [];
+    const events = [];
+
+    if (options.events?.length) {
+      options.events.forEach((event) => {
+        const newEvent = { id: event.id, start: event.start, end: event.end };
+        // Instruments are mapped to resources
+        if (event.instrument) {
+          const instrumentTitle = event.instrument;
+          let resource = resources.find((r) => r.title === instrumentTitle);
+
+          if (!resource) {
+            resource = { id: resources.length, title: instrumentTitle };
+            resources.push(resource);
+          }
+          newEvent.resourceId = resource.id;
+        }
+
+        // Proposals sets the color and title
+        if (event.proposal) {
+          const proposalTitle = event.proposal;
+          let proposal = proposals.find((p) => p.title === proposalTitle);
+
+          if (!proposal) {
+            proposal = { id: proposals.length, title: proposalTitle };
+            proposals.push(proposal);
+          }
+
+          newEvent.title = proposal.title;
+          newEvent.backgroundColor = CalendarElement._getColor(proposal.id);
+          newEvent.borderColor = CalendarElement._getColor(proposal.id);
+        }
+
+        if (event.type) {
+          newEvent.classNames = [`event--type-${event.type}`];
+
+          if (event.type === "reserved") {
+            newEvent.backgroundColor = CalendarElement.RESERVED_COLOR;
+            newEvent.borderColor = CalendarElement.RESERVED_COLOR;
+            newEvent.title = "Reserved";
+          }
+        }
+
+        // Concat event title
+        if (event.title) {
+          newEvent.title = newEvent.title
+            ? `${newEvent.title}: ${event.title}`
+            : event.title;
+        }
+
+        events.push(newEvent);
+      });
+    }
+
+    newOptions.resources = resources;
+    newOptions.events = events;
+
+    return newOptions;
   }
 
   _handleOptions(options) {
